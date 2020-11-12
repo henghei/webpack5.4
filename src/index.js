cd73537a7ea85a76e1f59208284526e134afa84f
henghei <1165104258@qq.com>
2020-11-12 17:26:51 +0800

    RemovPreviousLoader add HtmlWbpackPlugin CleanWebpackPlugin

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
  import _ from 'lodash'
- import './style.css'
- import Icon from './icon.png'
+ // import './style.css'
+ // import Icon from './icon.png'
+ import printMe from './print.js'
 
   function component() {
     const element = document.createElement('div');
+    const btn=document.createElement('button')
 
    // lodash（目前通过一个 script 引入）对于执行这一行是必需的
    // lodash，现在通过一个 script 引入
     element.innerHTML = _.join(['Hello', 'webpack','测试一下'], ' ');
-    element.classList.add('hello');
-    const myIcon=new Image()
+    btn.innerHTML='点击这里，然后查看console!'
+    btn.onclick=printMe
+    element.appendChild(btn)
+    // element.classList.add('hello');
+    /*const myIcon=new Image()
     myIcon.src=Icon
-    element.appendChild(myIcon)
+    element.appendChild(myIcon)*/
     return element;
   }
 
